4ab541bf7f7572df895e36dee47cb107c2cc7cdf
Chase <clecroy@calstart.org>
2020-06-20 16:39:15 -0700

    download and write malts, trim in excel, read malts

--- whisky_script.R
+++ whisky_script.R
@@ -2,35 +2,83 @@ library(tidyverse)
 library(lubridate)
 library(rvest)
 library(xml2)
+library(microbenchmark)
+library(parallel)
+library(textreuse)
 
-# read html of malt results
+### ADD COMMENTS
+
+#### EXPORT PACKAGES AND FUNCTIONS TO CLUSTERS
+
+##### TEST PARALELL RUN OF read_pages
+
+
+  # Read html of 500 variety == "malt" results
 kl <- read_html("https://www.klwines.com/Products?&filters=sv2_206!27&limit=500&offset=0&orderBy=60%20asc,search.score()%20desc&searchText=")
 
-# store full product names
-prod_name <- kl %>% html_nodes('.tf-product-header') %>% html_text()
+  # Extract product names
+prod_name <- kl %>% 
+  html_nodes('.tf-product-header') %>% 
+  html_text()
 
-# store TRUNCATED product descriptions from extended search results page
-prod_desc <- kl %>% html_nodes('.tf-product-description') %>% html_text()
-
-# get results block from body node, store all children nodes
+  # Get variety == "malt" results block from body node, store all children nodes
 res_blk <- kl %>% 
   html_nodes('body') %>% 
   xml_find_all("//div[contains(@class, 'results-block clearfix')]")%>% 
   html_children()
 
-# extract product ids as text from results block children nodes, remove names in vector
+  # Extract product ids as text from results block children nodes, remove names in vector
 ids <- res_blk[seq(1,length(res_blk),3)] %>% xml_attrs() %>% unlist()
 names(ids) <- NULL
 
-xml_try <- kl %>% 
-  html_nodes('body') %>% 
-  xml_find_all("//div[contains(@class, 'content')]")
-xml_try[[2]]
+# generate list of urls or each malt
+url_list <- paste0("https://www.klwines.com/p/i?i=",ids)
 
-xml_try2 <- kl %>% 
-  html_nodes('body-con') %>% html_children()
+# function to store html of each product page; delay each fetch by 5 seconds
+
+prod <- list_along(1:length(url_list))
+
+read_pages <- function(url_element) {
+  prod_html <- read_html(url_element)
+  
+  prod_desc <- prod_html %>% 
+    html_nodes('p') %>% 
+    html_text() %>% 
+    paste(collapse="|")
+  
+  prod_revs <- prod_html %>% 
+    html_nodes('.ReviewTextDetail') %>% 
+    html_text() %>% 
+    paste(collapse="|")
+  
+  Sys.sleep(5)
+  
+  return(c(prod_desc, prod_revs))
+}
 
 
+##### Call function
+########## NOTE this takes ~30 min to run ##########
 
-%>% html_text()
-head(xml_try2,50)
+malts_raw <- lapply(url_list, read_pages)
+
+# organize and store results as dataframe: id, name, desc, reviews
+malts <- data.frame("id" = as.numeric(ids))
+malts$name <- as.character(prod_name)
+malts$desc <- unlist(lapply(malts_raw, function(x) x[[1]]))
+malts$reviews <- unlist(lapply(malts_raw, function(x) x[[2]]))
+
+# save output
+write_csv(as.data.frame(malts), "C:/Users/clecroy/malts.csv")
+
+# detectCores()
+# 
+# cl <- makeCluster(6)
+# 
+# three_prod <- microbenchmark(parLapply(cl, url_list[1:6], read_pages))
+# 
+# stopCluster(cl)
+
+malts <- read_csv("C:/Users/clecroy/whisky/malts.csv")
+
+
